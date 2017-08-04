dd5c45f8915ae04f946ac62b04164d9062fce9bc
ryannewington <ryan@lithiumblue.com>
2017-08-05 08:52:32 +1000

    Add project files.

---
+++ Lithnet.ActiveDirectory.PasswordFilter.Native/Lithnet.ActiveDirectory.PasswordFilter.Native.h
@@ -0,0 +1,13 @@
+// Lithnet.ActiveDirectory.PasswordFilter.Native.h
+
+#pragma once
+
+using namespace System;
+
+namespace LithnetActiveDirectoryPasswordFilterNative {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
