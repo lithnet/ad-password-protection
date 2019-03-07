2138d5d8b0a3c6522d3b8af9b4918c55bc097872
ryannewington <ryan@lithiumblue.com>
2019-03-07 13:41:55 +1100

    Bumps version

--- src/PasswordProtection/Properties/AssemblyInfo.cs
+++ src/PasswordProtection/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.6993.4803")]
-[assembly: AssemblyVersion("1.0.6993.4803")]
-[assembly: AssemblyFileVersion("1.0.6993.4803")]
+// [assembly: AssemblyVersion("1.0.7005.4842")]
+[assembly: AssemblyVersion("1.0.7005.4842")]
+[assembly: AssemblyFileVersion("1.0.7005.4842")]
