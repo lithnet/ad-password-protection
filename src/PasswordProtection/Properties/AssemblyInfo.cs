9aefb740a420f9acd734b5ada2b62dc8224aad7e
Ryan Newington <ryan@lithiumblue.com>
2022-09-07 07:52:53 +1000

    Fixes an issue where the PowerShell module would not load due to the DSInternals library requiring VC runtime

--- src/PasswordProtection/Properties/AssemblyInfo.cs
+++ src/PasswordProtection/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lithnet")]
 [assembly: AssemblyProduct("Active Directory Password Protection")]
-[assembly: AssemblyCopyright("Copyright © Lithnet 2018")]
+[assembly: AssemblyCopyright("Copyright © Lithnet 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.7239.0")]
-[assembly: AssemblyVersion("1.0.7242.0")]
-[assembly: AssemblyFileVersion("1.0.7242.0")]
+[assembly: AssemblyVersion("1.0.7243.0")]
+[assembly: AssemblyFileVersion("1.0.7243.0")]
